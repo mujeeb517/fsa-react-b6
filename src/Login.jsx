aef3eaab7422a0423871f5b3f3a297a61e1e0e45
Mujib <mujib@Mujibs-MacBook-Pro.local>
2023-04-25 08:56:35 +0530

    Login api request

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,13 @@
 import { Component } from "react";
+import axios from 'axios';
 import ShouldRender from "./ShouldRender";
 
 class Login extends Component {
 
     state = {
-        username: '',
-        password: ''
+        email: '',
+        password: '',
+        hasError: false
     };
 
     onTextChange = (evt) => {
@@ -14,26 +16,36 @@ class Login extends Component {
         });
     }
 
-    onLogin = (evt) => {
-        console.log(this.state);
-        evt.preventDefault();
+    onLogin = async (evt) => {
+        try {
+            evt.preventDefault();
+            const res = await axios.post('https://fsa-api-b4.onrender.com/api/users/signin', this.state);
+            localStorage.setItem('token', res.data.token);
+        } catch (err) {
+            this.setState({ hasError: true });
+        }
     }
 
     isInvalid = () => {
-        return !this.state.username || !this.state.password;
+        return !this.state.email || !this.state.password;
     }
 
     render() {
         return <form onSubmit={this.onLogin}>
+            <ShouldRender condition={this.state.hasError}>
+                <div className="bg-red-500 p-2 m-2 text-white">
+                    Invalid username or password.
+                </div>
+            </ShouldRender>
             <div className="bg-white shadow-md w-1/3 rounded px-8 pt-6 pb-8 m-3">
                 <h1 className="text-2xl text-center font-semibold text-gray-500">Login</h1>
                 <div className="mb-4 mt-2 text-gray-500">
-                    <label className="block text-sm font-bold mb-2">Username</label>
-                    <input name="username" onChange={this.onTextChange}
-                        style={{ borderLeft: this.state.username ? '5px solid green' : '5px solid brown' }}
+                    <label className="block text-sm font-bold mb-2">Email</label>
+                    <input name="email" onChange={this.onTextChange}
+                        style={{ borderLeft: this.state.email ? '5px solid green' : '5px solid brown' }}
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Username" />
-                    <ShouldRender condition={!this.state.username}>
-                        <span className="text-red-400">Username required</span>
+                    <ShouldRender condition={!this.state.email}>
+                        <span className="text-red-400">Email required</span>
                     </ShouldRender>
                 </div>
                 <div className="mb-4">
